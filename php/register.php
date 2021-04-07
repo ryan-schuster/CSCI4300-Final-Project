31ce3af3e52fc621188c367a704d8903b37ea4e7
Ryan Schuster <rjs48792@uga.edu>
2021-04-06 20:58:31 -0400

    users can be now added to a database

--- php/register.php
+++ php/register.php
@@ -8,9 +8,12 @@
     try {
         $db = new PDO($dsn, $dbUsername, $dbPassword);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $query = "INSERT INTO users (name, email, userPassword)
-        VALUES ($name, $email, $password)";
-        $db->exec($query);
+        $query = $db->prepare("INSERT INTO users2 (name, email, userPassword) #change from users2 to users
+        VALUES (:name, :email, :password)");
+        $query->bindParam(':name', $name);
+        $query->bindParam(':email', $email);
+        $query->bindParam(':password', $password);
+        $query->execute();
         } catch (PDOException $e) {
         $error_message = $e->getMessage();
         echo "<p>An error occurred while connecting to the database: $error_message </p>";
