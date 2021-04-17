c2af1dc85aad15ed8023b35bbfe560be08a81eee
The-INTJ <raylotdreygone@gmail.com>
2021-04-17 00:44:37 -0400

    Tracker page CSS started

--- html/tracker.php
+++ html/tracker.php
@@ -14,7 +14,7 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/tracker.css">
     <title>TBD</title>
     <!-- <link rel="shortcut icon" src="../img/qstion.webp">
     <meta charset="utf-8">
@@ -30,7 +30,61 @@
         include $IPATH;
     ?>
 
-    
+    <div id="percent-better-cont">
+        <h1>69.96%!</h1>
+        <span></span>
+        <p id="better-txt"><em>Better Than Before</em></p>
+    </div>
+
+    <div id="tracker-boxes-cont">
+
+        <div id="new-achievements-cont">
+            <h2>New Achievments</h2>
+            <span class="separator"></span>
+            <p>+</p>
+            <div class="achieve">
+                <div class="circle"></div>
+                <p>An incredible thing</p>
+            </div>
+            <div class="complete-bar">
+                <span></span>
+                <p><em>Completed</em></p>
+                <span></span>
+            </div>
+            <div class="achieved">
+                <div class="circle"></div>
+                <p>An incredible thing</p>
+            </div>
+            <div class="achieved">
+                <div class="circle"></div>
+                <p>An incredible thing</p>
+            </div>
+        </div>
+
+        <div id="excellent-habits-cont">
+            <h2>Excellent Habits</h2>
+            <span class="separator"></span>
+            <p>+</p>
+            <div class="achieve">
+                <div class="circle"></div>
+                <p>An incredible thing</p>
+            </div>
+            <div class="complete-bar">
+                <span></span>
+                <p><em>Completed Today</em></p>
+                <span></span>
+            </div>
+            <div class="achieved">
+                <div class="circle"></div>
+                <p>An incredible thing</p>
+            </div>
+            <div class="achieved">
+                <div class="circle"></div>
+                <p>An incredible thing</p>
+            </div>
+        </div>
+
+    </div>
 
 </body>
 
