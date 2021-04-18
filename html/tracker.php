93797c3b987ac630178502de8971d8046c404e59
Ryan Schuster <rjs48792@uga.edu>
2021-04-18 18:56:32 -0400

    tracker displays goals

--- html/tracker.php
+++ html/tracker.php
@@ -24,10 +24,30 @@ if (isset($_SESSION["username"])) {
     foreach ($userInfo as $score) {
         $percent = $score['score'];
     }
+    include '../php/update.php';
+    $query = "SELECT goals.goalName FROM goalslist INNER JOIN goals
+    ON goals.goalID = goalslist.goalID WHERE goals.daily=0 AND $userID=userID AND completed=1";
+    $queryAchieveComp = $db->prepare($query);
+    $queryAchieveComp->execute();
+    $achCompGuard = true;
 
+    $query = "SELECT goals.goalName FROM goalslist INNER JOIN goals
+    ON goals.goalID = goalslist.goalID WHERE goals.daily=0 AND $userID=userID AND completed=0";
+    $queryAchieve = $db->prepare($query);
+    $queryAchieve->execute();
+    $achGuard = true;
 
+    $query = "SELECT goals.goalName FROM goalslist INNER JOIN goals
+    ON goals.goalID = goalslist.goalID WHERE goals.daily=1 AND $userID=userID AND completed=1";
+    $queryHabitComp = $db->prepare($query);
+    $queryHabitComp->execute();
+    $habitCompGuard = true;
 
-
+    $query = "SELECT goals.goalName FROM goalslist INNER JOIN goals
+    ON goals.goalID = goalslist.goalID WHERE goals.daily=1 AND $userID=userID AND completed=0";
+    $queryHabit = $db->prepare($query);
+    $queryHabit->execute();
+    $habitGuard = true;
 }
 
 ?>
@@ -64,46 +84,103 @@ if (isset($_SESSION["username"])) {
             <h2>New Achievments</h2>
             <span class="separator"></span>
             <a href="account/achieveAdderPage.php"><p class="plus">+</p></a>
-            <div class="achieve">
-                <div class="circle"></div>
-                <p>An incredible thing</p>
-            </div>
+            <?php if ($loggedIn) :?>
+            <?php foreach($queryAchieve as $a) {?>
+                <div class="achieve">
+                    <div class="circle"></div>
+                    <p><?php echo $a['goalName'];
+                    $achGuard = false;?></p>
+                </div>
+            <?php }?>
+            <?php if ($achGuard) :?>
+                <div class="achieve">
+                    <div class="circle"></div>
+                    <p>No achievements</p>
+                </div>
+                <?php endif;?>
+            <?php else : ?>
+                <div class="achieve">
+                    <div class="circle"></div>
+                    <p>Try logging in</p>
+                </div>
+            <?php endif;?>
             <div class="complete-bar">
                 <span></span>
                 <p><em>Completed Total</em></p>
                 <span></span>
             </div>
-            <div class="achieved">
-                <div class="circle"></div>
-                <p>An incredible thing</p>
-            </div>
-            <div class="achieved">
-                <div class="circle"></div>
-                <p>An incredible thing</p>
-            </div>
+            <?php if ($loggedIn) :?>
+            <?php foreach($queryAchieveComp as $aC) {?>
+                <div class="achieved">
+                    <div class="circle"></div>
+                    <p><?php echo $aC['goalName'];
+                    $achCompGuard = false;?></p>
+                </div>
+            <?php }?>
+            <?php if ($achCompGuard) :?>
+                <div class="achieved">
+                    <div class="circle"></div>
+                    <p>No Achievements</p>
+                </div>
+                <?php endif;?>
+            <?php else : ?>
+                <div class="achieved">
+                    <div class="circle"></div>
+                    <p>Try logging in</p>
+                </div>
+            <?php endif;?>
         </div>
 
         <div id="excellent-habits-cont">
             <h2>Excellent Habits</h2>
             <span class="separator"></span>
             <a href="account/habitAdderPage.php"><p class="plus">+</p></a>
-            <div class="achieve">
-                <div class="circle"></div>
-                <p>An incredible thing</p>
-            </div>
-
+            <?php if ($loggedIn) :?>
+            <?php foreach($queryHabit as $a) {?>
+                <div class="achieve">
+                    <div class="circle"></div>
+                    <p><?php echo $a['goalName'];
+                    $habitGuard = false;?></p>
+                </div>
+            <?php }?>
+                <?php if ($habitGuard) :?>
+                <div class="achieve">
+                    <div class="circle"></div>
+                    <p>No habits</p>
+                </div>
+                <?php endif;?>
+            <?php else : ?>
+                <div class="achieve">
+                    <div class="circle"></div>
+                    <p>Try logging in</p>
+                </div>
+            <?php endif;?>
+    
             <div class="complete-bar">
                 <span></span>
                 <p><em>Completed Today</em></p>
                 <span></span>
             </div>
-            <div class="achieved">
-                <div class="circle"></div>
-                <p>An incredible thing</p>
-            </div>
-            <div class="achieved">
-                <div class="circle"></div>
-                <p>An incredible thing</p>
+            <?php if ($loggedIn) :?>
+            <?php foreach($queryHabitComp as $a) {?>
+                <div class="achieved">
+                    <div class="circle"></div>
+                    <p><?php echo $a['goalName'];
+                    $habitCompGuard = false;?></p>
+                </div>
+            <?php }?>
+                <?php if ($habitCompGuard) :?>
+                <div class="achieved">
+                    <div class="circle"></div>
+                    <p>Try completing a goal</p>
+                </div>
+                <?php endif;?>
+            <?php else : ?>
+                <div class="achieved">
+                    <div class="circle"></div>
+                    <p>Try logging in</p>
+                </div>
+            <?php endif;?>
             </div>
         </div>
 
