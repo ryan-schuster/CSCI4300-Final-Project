23676fd2b3a1b35b4790c58533afe56bfa64ea7f
CorbinSpence <46639741+CorbinSpence@users.noreply.github.com>
2021-04-18 12:54:18 -0400

    Create footer.php

---
+++ php/footer.php
@@ -0,0 +1,9 @@
+<footer>
+  
+  <span class="support_links"><a href="#"> support_link_a </a></span>
+  <span class="support_links"><a href="#"> support_link_b </a></span>
+  <span class="support_links"><a href="#"> support_link_c </a></span>
+  
+  <span id="copyright"> ©SomeCompany </span>
+  
+</footer>
