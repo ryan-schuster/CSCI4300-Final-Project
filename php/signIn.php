5ebc52465efd7fd7cc1dc33da6a0e0e05fd5bf90
The-INTJ <raylotdreygone@gmail.com>
2021-04-20 13:52:17 -0400

    final changes for presentation branch

--- php/signIn.php
+++ php/signIn.php
@@ -24,7 +24,7 @@
             if ($guard) {
                 $_SESSION["username"] = $userFullName;
                 $_SESSION["userID"] = $userID;
-                include '../html/account/signedIn.html';
+                include $_SERVER["DOCUMENT_ROOT"] . '/CSCI4300-Final-Project/html/main.php';
             } else {
                 $_SESSION["error"] = "true";
                 header("Location:../html/account/signInPage.php"); 
