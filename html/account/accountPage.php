b8c18701f311618adbcff4079534940578f9ba9b
The-INTJ <raylotdreygone@gmail.com>
2021-04-21 11:31:09 -0400

    more css changes

--- html/account/accountPage.php
+++ html/account/accountPage.php
@@ -3,12 +3,24 @@
     $account = "Sign in";
     $link = "account/signInPage.php";
     $loggedIn = false;
+    $db;
+    $userID;
     if (isset($_SESSION["username"])) {
         $account = "Welcome, " . $_SESSION["username"];
+        $userID = $_SESSION["userID"];
         $link = "";
         $loggedIn = true;
-    } 
-    $userID = $_SESSION["userID"];
+        $dsn = 'mysql:host=localhost;dbname=bettereveryday';
+        $dbUsername = 'root';
+        $dbPassword = '';
+        try {
+            $db = new PDO($dsn, $dbUsername, $dbPassword);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            $error_message = $e->getMessage();
+            echo "<p>An error occurred while connecting to the database: $error_message </p>";
+        }
+    }
     $userInfo = $db->prepare("SELECT * FROM users WHERE $userID=userID");//has userid, name, email, phone, password, score
     $userInfo->execute();
     $userGoalslistInfo = $db->prepare("SELECT goals.goalName, goalslist.completed, goals.daily FROM goals INNER JOIN goalslist ON 
@@ -38,14 +50,27 @@
     ?>
     <div id="whole">
 	<form action="signIn.php" method="post">
-		<h1> Account Information </h1><br>
-		<label> Username: </label>
-		<input type="text" id="username" name="username" required><br>
-		<label> Email: </label>
-		<input type="email" id="email" name="email" required><br>
-		<label> Phone: </label>
+        <div id="inner-form-div">
+		<h1> Account Information </h1>
+
+        <div>
+		<label> Username </label>
+		<input type="text" id="username" name="username" required>
+        </div>
+
+        <div>
+		<label> Email </label>
+		<input type="email" id="email" name="email" required>
+        </div>
+
+        <div>
+		<label> Phone </label>
 		<input type="tel" id="phone" name="phone" required pattern="\d{3}[\-]\d{3}[\-]\d{4}" title="Example format: 111-111-1111"><br>
-		<input type="submit" id="change" value="Submit Changes">
+        </div>
+
+		<div onClick="document.forms['search-form'].submit();" id="change">
+            Submit Changes
+        </div>
 	</form>
 	</div>
     <?php 
