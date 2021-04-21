c1513903fefd85773da643a9864124c07f9424b8
Ryan Schuster <rjs48792@uga.edu>
2021-04-20 22:48:38 -0400

    fixed adding goals

---
+++ html/account/accountPage.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+    $account = "Sign in";
+    $link = "account/signInPage.php";
+    $loggedIn = false;
+    if (isset($_SESSION["username"])) {
+        $account = "Welcome, " . $_SESSION["username"];
+        $link = "";
+        $loggedIn = true;
+    } 
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../css/main.css">
+    <title>TBD</title>
+    <!-- <link rel="shortcut icon" src="../img/qstion.webp">
+    <meta charset="utf-8">
+    <meta name="description" content="A website offering ways of getting in shape!">
+    <meta name="keywords" content="fitness, shape up, strong, muscles, relax"> -->
+</head>
+
+<body>
+
+    <?php 
+        $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/CSCI4300-Final-Project/php/nav-bar.php";
+        include $IPATH;
+    ?>
+    <br>
+    <div>
+        <h1>Account</h1>
+    </div>
+
+    <?php 
+        $IPATH = $_SERVER["DOCUMENT_ROOT"] . "/CSCI4300-Final-Project/php/footer.php";
+        include $IPATH;
+    ?>
+
+</body>
+
+</html>
--- js/specifics.js
+++ js/specifics.js
@@ -1,12 +1,10 @@
 
 $(".achieve").click(function() {
 
-    var clickedGoal = $.trim($(this).children()[0].childNodes[0].data);
-
-
+        var itemID = parseInt($(this).attr('id'));
     $.post( 
         '../../php/goalListAdder.php', // location of your php script
-        {goalName: clickedGoal}, // any data you want to send to the script
+        {goalID: itemID}, // any data you want to send to the script
         function( data ){  // a function to deal with the returned information
             // Reload the page
             //window.location.href = "../tracker.php";
